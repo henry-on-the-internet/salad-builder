d7d1e325f78c891019ede3e614fb7eb6cd236c17
henry-on-the-internet <narrator0120@gmail.com>
2022-08-16 07:47:07 +1000

    useContext for user name

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import SaladMaker from './components/SaladMaker/SaladMaker';
+import UserContext from './components/User/User';
+
+const user = {
+  name: 'Salad Builder',
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
 
 function App() {
   return (
-    <>
+    <UserContext.Provider value={user}>
       <Navigation />
       <SaladMaker />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- src/components/SaladItem/SaladItem.js
+++ src/components/SaladItem/SaladItem.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from 'prop-types'
 import { createUseStyles } from "react-jss";
+
+import UserContext from "../User/User";
 
 const useStyles = createUseStyles({
     add: {
@@ -30,7 +32,8 @@ const useStyles = createUseStyles({
 
 export default function SaladItem({image, name}){
     const classes = useStyles();
-    const favorite = true;
+    const user = useContext(UserContext);
+    const favorite = user.favorites.includes(name);
     return(
         <div className={classes.wrapper}>
             <h3>
